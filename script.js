f48b16e70d9953f0472e0bce661df471e01d3c62
Naman1000 <91007344+Naman1000@users.noreply.github.com>
2025-04-09 16:57:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
 //your JS code here. If required.
+const form = document.getElementById("userform");
+
+form.addEventListener("submit", function(event){
+	event.preventDefault();
+
+	const name = document.getElementById("name").value.trim();
+	const age = parseInt(document.getElementById("age").value.trim(),10);
+
+	
+
+	if(name==="" || age===""){
+		alert("Please enter valid details.")
+	}else{
+		alert("Form submitted successfully!")
+	}
+
+	new Promise((resolve,reject)=>{
+		setTimeout(()=>{
+			if(age>18){
+				resolve(`Welcome, . You can vote`)
+			}
+			else{
+				reject(`Oh sorry . You aren't old enough.`)
+			}
+		}, 4000)
+	})
+	.then((message)=>{
+		alert(message)
+	})
+	.catch((errorMessage)=>{
+		alert(errorMessage)
+	})
+})
